899a7f4ed4cc6b6dbbc2ff7eb96d0e6a03e8e29b
willy3087 <william.duarte@muralis.com.br>
2024-12-02 14:32:49 -0300

    Atualiza configuração CORS para permitir origem local além da origem do frontend específico

--- api/index.py
+++ api/index.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cmexfront-hnq5ruasb-williams-projects-2c392421.vercel.app"],  # Permite todas as origens temporariamente
+    allow_origins=[
+        "https://cmexfront-hnq5ruasb-williams-projects-2c392421.vercel.app",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
